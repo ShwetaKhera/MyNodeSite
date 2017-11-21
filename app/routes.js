23994633ac3cb33b53ef61b2f154850ec3a32f55
ShwetaKhera <shwetakhera96@gmail.com>
2017-11-21 23:41:25 +0530

    MyNodeSite initial commit

---
+++ app/routes.js
@@ -0,0 +1,22 @@
+
+var express = require('express');
+var path    = require('path');
+
+var router = express.Router();
+
+module.exports = router;
+
+router.get('/', function(req,res){
+	res.render('pages/index');
+});
+
+router.get('/about', function(req,res){
+	res.render('pages/about');
+});
+
+router.get('/contact', function(req,res){
+	res.render('pages/contact');
+});
+router.post('/contact', function(req,res){
+	
+});
